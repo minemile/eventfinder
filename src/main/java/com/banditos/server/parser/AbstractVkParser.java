2a4a3c1282323482b43a838ddf0b1df2850a05d5
coffman21 <fire2low@rambler.ru>
2018-02-20 22:27:03 +0300

    little fixes in parsers

--- src/main/java/com/banditos/server/parser/AbstractVkParser.java
+++ src/main/java/com/banditos/server/parser/AbstractVkParser.java
@@ -13,7 +13,10 @@ import com.vk.api.sdk.objects.wall.WallPostFull;
 import com.vk.api.sdk.objects.wall.responses.GetExtendedResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.data.domain.PageRequest;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
@@ -33,14 +36,13 @@ public abstract class AbstractVkParser {
         this.vk = new VkApiClient(transportClient);
 
         Integer app_id = Integer.valueOf(env.getProperty("vk.appid"));
-        String token = env.getProperty("vk.token");
+        String token = env.getProperty("token.vk");
         this.actor = new ServiceActor(app_id, token);
 
         this.tusovkaRepository = tusovkaRepository;
     }
 
-    public void getTusovkas() throws ApiException, ClientException
-    {
+    public void getTusovkas() throws ApiException, ClientException, MalformedURLException {
         GetExtendedResponse response = vk.wall().getExtended(actor).domain(domain).count(5).execute();
         List<Tusovka> tusovkas = new ArrayList<>();
         List<GroupFull> lgf = response.getGroups();
@@ -48,14 +50,20 @@ public abstract class AbstractVkParser {
         for (WallPostFull wpf : response.getItems())
         {
             GroupFull gf = lgf.get(i);
-            tusovkas.add(new Tusovka(Date.from(Instant.ofEpochSecond(wpf.getDate())), gf.getName(), wpf.getText(), "Таганка", 0));
+            tusovkas.add(new Tusovka(
+                    Date.from(Instant.ofEpochSecond(wpf.getDate())),
+                    gf.getName(),
+                    wpf.getText(),
+                    "Таганка",
+                    new URL("https://xui.tebe"),
+                    0));
             i++;
         }
         tusovkaRepository.save(tusovkas);
     }
 
     private Date getLastTusovka(String place) {
-        Tusovka tusovka = tusovkaRepository.findByPlaceOrderByDateAsc(place);
-        return tusovka.getDate();
+        List<Tusovka> tusovka = tusovkaRepository.findByPlaceOrderByDateDesc(place, new PageRequest(0, 1));
+        return tusovka.get(0).getDate();
     }
 }
--- src/main/java/com/banditos/server/parser/ParserScheduler.java
+++ src/main/java/com/banditos/server/parser/ParserScheduler.java
@@ -1,5 +1,7 @@
 package com.banditos.server.parser;
 
+import com.banditos.server.bot.geolocation.GoogleMapsApi;
+import com.google.maps.model.LatLng;
 import com.vk.api.sdk.exceptions.ApiException;
 import com.vk.api.sdk.exceptions.ClientException;
 import org.slf4j.Logger;
@@ -7,6 +9,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 @Component
 public class ParserScheduler {
@@ -17,11 +22,9 @@ public class ParserScheduler {
     private Logger logger = LoggerFactory.getLogger(ParserScheduler.class);
 
     //@Scheduled(cron = "* 20 16 ? * *")
-    @Scheduled(cron = "0 * * ? * *")
-    public void runParsers() throws ClientException, ApiException {
+    @Scheduled(cron = "*/5 * * ? * *")
+    public void runParsers() throws ClientException, ApiException, MalformedURLException {
         logger.info("Scheduled task started");
-
-        vkParser.getTusovkas();
 
     }
 }
